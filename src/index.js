af549b291e5f062173c7f627805ca8e6cd0dcea8
givennp <givennatio2@gmail.com>
2022-03-23 14:45:58 +0700

    delete lebih dari satu employee

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express();
+const { cors } = require("cors");
+const dotenv = require("dotenv")
+
+dotenv.config();
+
+
+app.use(express.json())
+
+const PORT = process.env.PORT
+
+const { employeeRoutes, authRoutes } = require("./routes");
+
+
+app.use("/employees", employeeRoutes)
+app.use("/auth", authRoutes)
+
+
+
+
+app.listen(PORT, () => {
+  console.log("Listening in port", PORT)
+})
